868fd36d56fc78717c1dfb2c5c0c015968d59947
Priyansh Singh <singhpriyansh2014@gmail.com>
2026-06-13 11:33:24 +0530

    Add types

---
+++ types/resume.ts
@@ -0,0 +1,14 @@
+export interface PersonalInfo {
+  name: string;
+  email: string;
+  phone: string;
+  location: string;
+  linkedin: string;
+  github: string;
+}
+
+export interface ResumeState {
+  personalInfo: PersonalInfo;
+  summary: string;
+  skills: string[];
+}
